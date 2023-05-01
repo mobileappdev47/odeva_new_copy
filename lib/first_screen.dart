768849e37312bf4b11d40712879e0b9f4df48bcd
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-05-01 18:08:29 +0530

    change j

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eshop/SignInUpAcc.dart';
 import 'package:eshop/web_view_screen.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,30 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  bool isHide;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+      firestore.collection("accountHide").get().then((value) {
+        print(value);
+
+        for(int i=0; i<value.docs.length; i++){
+          isHide = value.docs[i]["isHide"];
+          setState(() {});
+        }
+
+      });
+
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,56 +113,63 @@ class _FirstScreenState extends State<FirstScreen> {
                 SizedBox(
                   height: 40,
                 ),
-                GestureDetector(
-                  onTap: () async{
-                    // await launchUrl(Uri.parse("https://businesspartnershipportal.com"));
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => SignInUpAcc(),));
-                  },
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                        color: colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.deepPurple,
-                            blurRadius: 30,
-                            spreadRadius: 2,
-                          ),
-                        ],
-                      image: DecorationImage(
-                        image:  NetworkImage("https://media.istockphoto.com/id/1407787199/photo/portrait-of-young-asian-business-woman-using-digital-tablet-in-the-office.jpg?b=1&s=170667a&w=0&k=20&c=ASdZd3bww5_D6a5P5cOc3mA1k-rMeBImSj7YMwxtv2I="),fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 35,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.deepPurple,
-                          blurRadius: 50,
-                          spreadRadius: 0.5,
-                        ),
-                      ]),
-                  child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        "My Account",
-                        style: TextStyle(
-                            color: colors.darkColor,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      )),
-                ),
+
+                (isHide == true)
+                    ? Column(
+                 children: [
+                   GestureDetector(
+                     onTap: () async{
+                       // await launchUrl(Uri.parse("https://businesspartnershipportal.com"));
+                       Navigator.push(context, MaterialPageRoute(builder: (context) => SignInUpAcc(),));
+                     },
+                     child: Container(
+                       height: 150,
+                       width: 150,
+                       decoration: BoxDecoration(
+                         color: colors.white,
+                         borderRadius: BorderRadius.circular(20),
+                         boxShadow: [
+                           BoxShadow(
+                             color: Colors.deepPurple,
+                             blurRadius: 30,
+                             spreadRadius: 2,
+                           ),
+                         ],
+                         image: DecorationImage(
+                           image:  NetworkImage("https://media.istockphoto.com/id/1407787199/photo/portrait-of-young-asian-business-woman-using-digital-tablet-in-the-office.jpg?b=1&s=170667a&w=0&k=20&c=ASdZd3bww5_D6a5P5cOc3mA1k-rMeBImSj7YMwxtv2I="),fit: BoxFit.cover,
+                         ),
+                       ),
+                     ),
+                   ),
+                   SizedBox(
+                     height: 10,
+                   ),
+                   Container(
+                     height: 35,
+                     width: 170,
+                     decoration: BoxDecoration(
+                         color: colors.white,
+                         borderRadius: BorderRadius.circular(20),
+                         boxShadow: [
+                           BoxShadow(
+                             color: Colors.deepPurple,
+                             blurRadius: 50,
+                             spreadRadius: 0.5,
+                           ),
+                         ]),
+                     child: Align(
+                         alignment: Alignment.center,
+                         child: Text(
+                           "My Account",
+                           style: TextStyle(
+                               color: colors.darkColor,
+                               fontSize: 20,
+                               fontWeight: FontWeight.bold),
+                         )),
+                   ),
+                 ],
+               )
+                    : SizedBox(),
               ],
             ),
           ),
